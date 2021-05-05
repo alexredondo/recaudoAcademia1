8e43fa94cca48cbb132a4fcd659caa63772796ce
alexredondo <academia.americas@gmail.com>
2021-05-05 07:31:59 -0500

    version 0.05052021

--- estadoCuenta/estadoCuentaBot.py
+++ estadoCuenta/estadoCuentaBot.py
@@ -2,18 +2,6 @@
 # pylint: disable=C0116
 # This program is dedicated to the public domain under the CC0 license.
 
-"""
-First, a few callback functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Example of a bot-user conversation using ConversationHandler.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
 
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
@@ -28,7 +16,6 @@ from telegram.ext import (
 
 import responses as R
 
-print("Bot started")
 # Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -40,8 +27,11 @@ def start_command(update: Update, _: CallbackContext):
     update.message.reply_text("Type something random to get started!")
 
 def help_command(update: Update, _: CallbackContext):
-    update.message.reply_text("If you need help! google")
+    update.message.reply_text("Pronto te ayudaremos o selecciona la opción más adecuada: \n 🔹 Medios de pago: /mediodepago", parse_mode="markdown")
 
+def mediodepago_command(update: Update, _: CallbackContext):                   
+    update.message.reply_text("Opción 1️⃣ : Para pagar por internet puede usar este : \n [link de pago por PSE](https://www.mipagoamigo.com/MPA_WebSite/ServicePayments/StartPayment?id=4510&searchedCategoryId=&searchedAgreementName=ACADEMIA%20DE%20LAS%20AMERICAS)\n\nOpción 2️⃣: 1. Ir a la oficina *SERVICER* que le quede más cerca. \n 2. Suministra el número del convenio *15754127* \n 3. Suministra el NUIP del estudiante. (Registro civil o Tarjeta de identidad) \n\nOpción 3️⃣: 1. Ir a la sucursal del *BANCO CAJA SOCIAL*. \n 2. Suministra el número del convenio *15754127* \n 3. Suministra el NUIP del estudiante. (Registro civil o Tarjeta de identidad)", parse_mode="markdown")
+    
 def handle_message(update, context):
         text = str(update.message.text).lower()
         response = R.sample_responses(text)
@@ -56,12 +46,13 @@ def main() -> None:
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start_command))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("mediodepago", mediodepago_command))
     dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
 
 
     # Start the Bot
     updater.start_polling()
-
+    print("Bot started")
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
@@ -70,4 +61,3 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-
--- estadoCuenta/responses.py
+++ estadoCuenta/responses.py
@@ -1,28 +1,97 @@
+from random import choice
 import pandas as pd
 import json
+import os #eliminar archivos. ¿para qué más?
 
 def sample_responses(input_text):
     user_message = str(input_text).lower()
 
-    if user_message in ("hello", "hi",):
-        return "Hey"
-    if user_message in ("hola", ):
-        return "Que tal"
+    if user_message in ("hello", "hi","hola","buenos días", "buenas noches","buenas tardes","good morning","good morning academia",):
+        return "👋 Hola, cordial saludo!"
+    if user_message in ("estado de cuenta", ):
+        return  "Por favor escirba el NUIP del estudiante..." 
 
-    if user_message in ("estado de cuenta","Estado de cuenta"):
-
-        return "nuip"
-
-    if user_message in ("1138678313"):
-        id_est = int(input_text)
+    if user_message in ["1030247719","1103754527","1103515254","1025555579","1102847991","1103510179","1043481075","1103515205","1103748757","1138677533","1103757793","1103750555","1102860979","1068435161","1103513122","1104265422","1104263305","1202214087","1103509838","1102872411","1090486177","1103754409","1202213348","1138674376","1103756941","1027808387","1103753690","1103756140","1104267486","1103508124","1103758561","1202213742","1104264591","1103507608","1102873753","1104263333","1224953137","1102856958","1103748258","1138674413","1104265131","1103749236","1202213106","1103752461","1202213620","1103512107","1020123319","1103519561","1231338199","1104267664","1103513624","1103750756","1103506939","1103749833","1103511845","1138674754","1138674581","1202213738","1103515645","1102869604","1103517925","1102860504","1030246017","1043313558","1138676669","1103508213","1102878043","1103543553","1138677181","1102313645","1202213468","1139434087","1103512911","1104266694","1042263697","1103748574","1103753551","1125230534","1202213662","1103752312","1104265781","1058357747","1103508816","1103751941","1138674184","1202214233","1103515651","1103507961","1103752763","1103513363","1138677933","1104265537","1103749443","1201234938","1104262917","1103748954","1103748317","1100397739","1048081459","1138675306","1138677367","1202213870","1104262947","1103757256","1102850356","1138675305","1103516148","1103748035","1025902842","1103511781","1103509583","1102232626","1100626708","1103752247","1102868925","1103750026","1103513838","1023530668","1103750732","1138678313","1104263365","1103520704","1138674273","1104268355","1103514224","1030243055","1030245715","1104264968","1103754219","1138677262","1052245125","1104264601","1103511714","1103755533","1138677875","1103512840","1102874689","1202214022","1202213766","1102869539","1103756663","1103751028","1103508084","1103509334","1103507287","1103754928","1102872914","1043463150","1138674856","1103510317","1104268067","1102849604","1138674419","1103509948","1103512318","1103509750","1104264025","1103518629","1103748906","1103510808","1231340085","1103509444","1104266203","1103508560","1104262857","1103513759","1103514312","1104262997","1104267019","1103514744","1103509587","1103756114","1187464478","1103514320","1102849638","1103756595"]:
         recaudoPD = pd.read_csv ('https://docs.google.com/spreadsheets/d/e/2PACX-1vSqcAIkMJVJi5ehJB9TtTaRktBfj9eTLXizEZQAUQThOXmLx-k8k4w686ZpP0fEbwzIkKryYc4SW3Dt/pub?gid=1830933640&single=true&output=csv')
+        id_est=int(input_text)
         filtro = recaudoPD["ID"] == id_est
         my_filtro = recaudoPD[filtro]
-        print(my_filtro.sort_values("Fecha"))
-        return "este es mi *texto*",{parse_mode : "Markdown"}
-    #my_filtro.to_json()
+        myFiltroSort = my_filtro.sort_values("Fecha")
+
+        file1 = myFiltroSort[["Fecha","Pago","Valor","Concepto"]]
+
+        f = open("recaudo.txt","w")
+        f.write("🇪​​​​​🇸​​​​​🇹​​​​​🇦​​​​​🇩​​​​​🇴​​​​  🇨​​​​​🇺​​​​​🇪​​​​​🇳​​​​​🇹​​​​​🇦​​​"+"\n\n"+str("Estudiante:  "+my_filtro.iloc[1,1])+'\n\n')
+        f.write("Conceptos por pagar => 🔴"+"\n"+"Pagos realizados => 🟢"+"\n\n")
+        f.write("   Fecha    |    Valor    |    Concepto"+'\n')
+        archivoJson = file1.to_json(orient="split")
+        parsed = json.loads(archivoJson)
+
+        with open('recaudo.json','w') as f:
+            json.dump(parsed,f, indent=4)
 
 
+        with open ('recaudo.json','r') as f:
+            recaudo_json =json.load(f)
 
-    return "No tengo respuesta"
+        for i in range(len(recaudo_json["data"])):
+            print (recaudo_json["data"][i])
+
+            with open ('recaudo.txt','a') as f:
+                f.write("__________________________________"+'\n')
+                f.write(str(recaudo_json["data"][i])+'\n')
+
+        f=open("recaudo.txt","a")
+        totalCostos = myFiltroSort["Valor"].sum()
+        totalPagos = myFiltroSort["Pago"].sum()
+        pendiente = totalCostos-totalPagos
+        f.write("\n")
+        f.write("Costos Educativos: {:,} ".format(totalCostos))
+        f.write("\n")
+        f.write("🟢 Pagos Realizados: {:,}".format(totalPagos))                 
+        f.write("\n")                                                          
+        f.write("🔴 Pendiente por pagar: {:,}".format(pendiente))              
+        f.write("\n"+"Ir al siguiente link para acceder a los medios de pago: "+"\n")
+        f.write("/mediodepago")
+        f.close()
+
+        registro=open("recaudo.txt","r")
+        f = open("recaudoText.txt","w")
+        for x in registro:
+            xf=x.replace("]","")
+            f.write(xf)
+        f.close()
+
+        registro=open("recaudoText.txt","r")
+        f = open("recaudoText1.txt","w")
+        for x in registro:
+            xf=x.replace("[","")
+            f.write(xf)
+        f.close()
+
+        registro=open("recaudoText1.txt","r")
+        f = open("recaudoText2.txt","w")
+        for x in registro:
+            xf=x.replace(", ","  |  ")
+            f.write(xf)
+        f.close()
+
+        registro=open("recaudoText2.txt","r")
+        f = open("recaudoText3.txt","w")
+        for x in registro:
+            xf=x.replace("\'","")
+            f.write(xf)
+        f.close()
+
+        registro=open("recaudoText3.txt","r")
+        f = open("recaudoText4.txt","w")
+        for x in registro:
+            xf=x.replace(" 0 ","")
+            f.write(xf)
+        f.close()
+
+        f = open("recaudoText4.txt","r") 
+        return f.read()
+    aleatorio = choice(["/help 💬","🟠🟢🔵🔴","","","","",""])
+    return aleatorio
 
