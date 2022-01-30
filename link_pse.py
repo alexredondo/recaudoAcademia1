8cb5b9246485c67463905b1af9358c883939311f
Alexander <alexander.red@gmail.com>
2022-01-30 16:33:24 -0500

    Actualización mejora general

--- link_pse.py
+++
@@ -1,5 +0,0 @@
-def link_pse_command(update: Update, _: CallbackContext):
-    update.message.reply_text("""\n\n
-    🏦 Para pagar por internet puede usar este : \n
-    [link de pago por PSE](https://www.mipagoamigo.com/MPA_WebSite/ServicePayments/StartPayment?id=4510&searchedCategoryId=&searchedAgreementName=ACADEMIA%20DE%20LAS%20AMERICAS)"""
-        , parse_mode="markdown")
