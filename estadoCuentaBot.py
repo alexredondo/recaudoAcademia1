3c60d0167a85e642dee9d36f27a3475c5ba356a6
Alexander <alexander.red@gmail.com>
2022-01-30 17:01:15 -0500

    Actualización mejora general

--- estadoCuentaBot.py
+++ estadoCuentaBot.py
@@ -77,8 +77,8 @@ def escuelapadres_command(update: Update, _: CallbackContext):
     update.message.reply_text("""Cordial saludo familia !\n
     Bienvenidos a nuestra escuela de padres.\n\n
     *TABLA DE CONTENIDO*:\n
-    /capitulo1 -mes de febrero {year} -\n
-    Dios piensa en individuos, naciones y generaciones.\n
+    [Capítulo 1 - Dios piensa en individuos, naciones y generaciones.](https://telegra.ph/Dios-piensa-en-individuos-naciones-y-generaciones-01-30)\n
+    \n
     /capitulo2 -Mes de marzo -\n
     Dios instruye la familia y Sus leyes para ella.
     /capitulo3 -Mes de abril -\n
@@ -86,17 +86,17 @@ def escuelapadres_command(update: Update, _: CallbackContext):
     /capitulo4 -Mes de mayo -\n
     Dios piensa en individuos, naciones y generaciones.\n
     /capitulo5 -Mes de junio -\n
-    Dios piensa en individuos, naciones y generaciones.\n\n
+    Dios piensa en individuos, naciones y generaciones.\n
     /capitulo6 -Mes de julio -\n
-    Dios piensa en individuos, naciones y generaciones.\n\n
+    Dios piensa en individuos, naciones y generaciones.\n
     /capitulo7 -Mes de agosto -\n
-    Dios piensa en individuos, naciones y generaciones.\n\n
+    Dios piensa en individuos, naciones y generaciones.\n
     /capitulo8 -Mes de septiembre -\n
-    Dios piensa en individuos, naciones y generaciones.\n\n
+    Dios piensa en individuos, naciones y generaciones.\n
     /capitulo9 -Mes de octubre -\n
-    Dios piensa en individuos, naciones y generaciones.\n\n
+    Dios piensa en individuos, naciones y generaciones.\n
     /capitulo10 -Mes de noviembre -\n
-    Dios piensa en individuos, naciones y generaciones.\n\n    
+    Dios piensa en individuos, naciones y generaciones.\n
 """, parse_mode="markdown")
 
 # Medios de pago /mediodepago
