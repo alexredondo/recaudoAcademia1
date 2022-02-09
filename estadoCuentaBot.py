c23080a79e7fa2ca7cd67f98021ec10ed1555392
Alexander Redondo <alexander.red@gmail.com>
2022-02-09 12:52:21 -0500

    Update estadoCuentaBot.py

--- estadoCuentaBot.py
+++ estadoCuentaBot.py
@@ -126,7 +126,7 @@ def estadocuenta_command(update: Update, _: CallbackContext):
     id_chat_guion = str(update.effective_chat.id)
     id_chat_string = id_chat_guion.replace("-", "")
     recaudoPD = pd.read_csv ('https://docs.google.com/spreadsheets/d/e/2PACX-1vQyGXMDTSbh_vXeYVpkFF91ARGNYMKvYM27LfuFn35SJ78ja7ARPIhlQ9GU_hUOz596HIfQLo9L45_u/pub?gid=330651820&single=true&output=csv')
-    id_est=int(user_message)
+    id_est=int(id_chat_string)
     recaudoPD["Pago"] = pd.Series(recaudoPD["Pago"])
     recaudoPD["Pago"] = pd.to_numeric(recaudoPD["Pago"], downcast='integer')
     recaudoPD["Valor"] = pd.Series(recaudoPD["Valor"])
