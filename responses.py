bb68aa41cd1d0f8aa58c675d2e5a3e2b3b1a4241
Alexander Redondo <alexander.red@gmail.com>
2022-03-18 08:17:37 -0500

    Update responses.py

--- responses.py
+++ responses.py
@@ -336,16 +336,6 @@ Hola 🌺,\n\n Para conocer los medios de pago vaya a /mediodepago\n"""
 
 ### FIN LISTA DOCENTES ###
 
-### START MENSAJE DE COBRO ###
-    id_chat_guion = str(update.effective_chat.id)
-    id_chat_string = id_chat_guion.replace("-", "")
-    id_est=int(id_chat_string)
-    if id_est in [741245286]:
-        return """Hola familia ! \n
-        Se registra pago pendiente con Academia de las Américas,\n por favor revise su estado de cuenta\n
-        /cuenta"""
-### END MENSAJE DE COBRO ###
-
 #Default 
     aleatorio = ""#choice(["/help 💬","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","🟠🟢🔵🔴",""])
     
